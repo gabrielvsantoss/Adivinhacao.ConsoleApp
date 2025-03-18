72ad6b797ecca3df30cad5ddb5e159db9673bf62
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-18 01:35:44 -0300

    Programacao Estruturada

---
+++ Adivinhacao.ConsoleApp/Adivinhacao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adivinhacao.ConsoleApp
+{
+    class Adivinhacao
+    {
+    }
+}
